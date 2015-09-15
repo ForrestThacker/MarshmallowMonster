239f3cea7554a4769f864df7719142cfd69a2dba
ForrestThacker <ftha4681@740s-pl-116.local>
2015-09-15 13:01:05 -0600

    Added getters/Setters

--- src/marshmallows/model/MarshmallowMonster.java
+++ src/marshmallows/model/MarshmallowMonster.java
@@ -17,13 +17,33 @@ public class MarshmallowMonster
 	public MarshmallowMonster(String monsterName, int monsterEyes,
 								 int monsterNoses, boolean monsterBellybutton,
 								double monsterLegs, double monsterHair)
-	{									
+								{
 		this.monsterName = monsterName;
 		this.monsterEyes = monsterEyes;
 		this.monsterBellybutton = monsterBellybutton;
 		this.monsterNoses = monsterNoses;
 		this.monsterHair = monsterHair;
 		this.monsterLegs = monsterLegs;
+	}
+								
+	public String getMonsterName()
+	{
+		return monsterName;
+	}
+	
+	public int getMonsterEyes()
+	{
+		return monsterEyes;
+	}
+	
+	public void setMonsterName(String monsterName)
+	{
+		this.monsterName = monsterName;
+	}
+	
+	public void setMonsterEyes(int monsterEyes)
+	{
+		this.monsterEyes = monsterEyes;
 	}
 	
 	public String toString()
